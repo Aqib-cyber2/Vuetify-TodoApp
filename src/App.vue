ac0556b958ce5266927bfa343f09305f507018ed
Muhammad Aqib <aqibafridi0345@gmail.com>
2022-07-06 03:10:35 +0500

    Vuetify todo app Frontend

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-app :style="{background: $vuetify.theme.themes[theme].secondary}">
+    <v-navigation-drawer app v-model="drawer" color="#005466" dark>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            TODO App
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+
+        </v-list-item>
+
+      </v-list>
+
+    </v-navigation-drawer>
+
+
+    <v-app-bar app color="primary" dark src="header.jpg" prominent>
+
+      <template v-slot:img="{ props }">
+        <v-img v-bind="props" gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click.stop="toggle"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-app-bar-title class="mx-auto">
+        <p>Vuetify Todo App</p>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+
+</template>
+
+<script>
+  export default {
+    name: 'App',
+
+    data: () => {
+      return {
+        drawer: null,
+        items: [{
+            title: 'Dashboard',
+            icon: 'mdi-format-list-checks',
+            to: '/'
+          },
+          {
+            title: 'About',
+            icon: 'mdi-help-box',
+            to: '/about'
+          },
+        ],
+      }
+    },
+
+    computed: {
+      theme() {
+        return (this.$vuetify.theme.dark) ? 'dark' : 'light'
+      }
+    },
+
+    methods: {
+      toggle() {
+        this.drawer = !this.drawer
+        console.log(this.drawer)
+      }
+    }
+  };
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,135 @@
+<template>
+
+
+  <v-row class="mt-7">
+
+    <v-col cols="9 mx-auto" class="mb-0">
+      <v-toolbar elevation="0" flat>
+        <v-toolbar-title> User List </v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <v-dialog v-model="dialog" persistent max-width="400px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="#005466" dark v-bind="attrs" v-on="on">
+              Add new User
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">User Profile</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field hide-details label="Full Name*" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field hide-details label="Username"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="dialog = false">
+                Close
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="dialog = false">
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-simple-table fixed-header color="purple lighten-5" class="pa-3">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Avator</th>
+              <th>Full Name</th>
+              <th>
+                Username
+              </th>
+              <th>
+                Action
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name">
+
+              <td>{{ item.id }}</td>
+
+              <td>
+                <v-avatar size="30">
+                  <img v-bind:src="item.avator" alt="user avator">
+                </v-avatar>
+              </td>
+
+              <td>{{ item.name }}</td>
+
+              <td>{{ item.username }}</td>
+
+              <td>
+                <v-btn fab dark x-small outlined color="blue darken-1">
+                  <v-icon dark small>
+                    mdi-account-edit
+                  </v-icon>
+                </v-btn>
+                <v-btn class="ml-2" fab dark x-small color="red darken-4">
+                  <v-icon dark small>
+                    mdi-delete
+                  </v-icon>
+                </v-btn>
+              </td>
+
+            </tr>
+
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-col>
+
+  </v-row>
+
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => {
+      return {
+        dialog: false,
+        desserts: [{
+            id: '1',
+            avator: 'https://cdn.vuetifyjs.com/images/john.jpg',
+            name: 'Frozen Yogurt',
+            username: 'user1@gmail.com',
+            calories: 159,
+          },
+          {
+            id: '1',
+            avator: 'https://cdn.vuetifyjs.com/images/john.jpg',
+            name: 'Frozen Yogurt',
+            username: 'user1@gmail.com',
+            calories: 159,
+          },
+          {
+            id: '1',
+            avator: 'https://cdn.vuetifyjs.com/images/john.jpg',
+            name: 'Frozen Yogurt',
+            username: 'user1@gmail.com',
+            calories: 159,
+          }
+        ]
+      }
+    }
+  }
+</script>
